15df7b424118a38d8c4f1a2bba5c9430d7f3f666
Devin Godage <devinkanilka@gmail.com>
2021-09-20 03:31:02 +0530

    Passing function as a prop

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,6 +1,6 @@
 const BlogList = (props) => {
-    const { blogs, title } = props;
-
+    const { blogs, title, handleDelete } = props;
+    
     return (
         <div className="blog-list">
             <h2>{title}</h2>
@@ -8,6 +8,7 @@ const BlogList = (props) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
+                    <button onClick={() => handleDelete(blog.id)}>delete blog</button>
                 </div>
             ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -5,12 +5,17 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'Devin', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'DeeKay', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'TeeJay', id: 1 }
-    ])
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'TeeJay', id: 3 }
+    ]);
+
+    const handleDelete = (blogId) => {
+        const newBlogs = blogs.filter(blog =>  blog.id !== blogId)
+        setBlogs(newBlogs)
+    }
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs"/>
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
         </div>
     );
 }
