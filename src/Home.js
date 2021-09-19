d969c17791625e8465753e5e39571fa76ca346c7
Devin Godage <devinkanilka@gmail.com>
2021-09-19 19:57:38 +0530

    HomePage added

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
-    return ( <div className="home">
+    const handleClick = (e) => {
+        console.log("Hello Devin", e)
+    }
+
+    const heandleClickAgain = (name) => {
+        console.log('hello ' + name);
+    }
+
+    return (<div className="home">
         <h1>Home Page</h1>
-    </div> );
+        <button onClick={handleClick}>Click Me</button>
+        <button onClick={() => heandleClickAgain('Deekay')}>Click Me Again</button>
+    </div>);
 }
- 
+
 export default Home;
