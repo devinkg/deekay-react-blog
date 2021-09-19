ffb2eae086ae92316d7004ebb14e4e1bbb704295
Devin Godage <devinkanilka@gmail.com>
2021-09-19 23:01:18 +0530

    useState Hook used

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,18 @@
-const Home = () => {
-    const handleClick = (e) => {
-        console.log("Hello Devin", e)
-    }
+import React, { useState } from "react";
 
-    const heandleClickAgain = (name) => {
-        console.log('hello ' + name);
+const Home = () => {
+    const [name, setName] = useState('Devin');
+    const [age, setAge] = useState(25);
+
+    const handleClick = () => {
+        setName("DeeKay");
+        setAge(26);
     }
 
     return (<div className="home">
         <h1>Home Page</h1>
+        <p>{name} is {age} years old</p>
         <button onClick={handleClick}>Click Me</button>
-        <button onClick={() => heandleClickAgain('Deekay')}>Click Me Again</button>
     </div>);
 }
 
