877af852302a558db3b516e3d9e0866b845c56c7
Devin Godage <devinkanilka@gmail.com>
2021-09-21 01:47:16 +0530

    useEffect fetch data added and DB updated

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'Devin', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'DeeKay', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'TeeJay', id: 3 }
-    ]);
+    const [blogs, setBlogs] = useState([]);
 
     const handleDelete = (blogId) => {
-        const newBlogs = blogs.filter(blog =>  blog.id !== blogId)
+        const newBlogs = blogs.filter(blog => blog.id !== blogId)
         setBlogs(newBlogs)
     }
 
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json()
+            }).then((data) => {
+                setBlogs(data)
+            }).catch((err) => {
+                alert(err)
+            })
+    }, [])
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
         </div>
     );
 }
