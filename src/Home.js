fa94b66e1fa0c06b8c91bd565ed0bb90b04a14c6
Devin Godage <devinkanilka@gmail.com>
2021-09-21 02:29:38 +0530

    Error and Loading displaying functions added

--- src/Home.js
+++ src/Home.js
@@ -2,22 +2,32 @@ import React, { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([]);
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
             .then(res => {
-                return res.json()
+                if (!res.ok) {
+                    throw Error('Could not fetch the data for that resource')
+                }
+                return res.json();
             }).then((data) => {
-                setBlogs(data)
+                setBlogs(data);
+                setIsPending(false);
+                setError(null);
             }).catch((err) => {
-                alert(err)
-            })
+                setIsPending(false);
+                setError(err.message);
+            });
     }, [])
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" />
+        {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
     );
 }
