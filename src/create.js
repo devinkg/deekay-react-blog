e00e546c5b64de78691ba77542eabd609380a286
Devin Godage <devinkanilka@gmail.com>
2021-09-22 01:48:05 +0530

    Add blog form created and styling added

--- src/create.js
+++ src/create.js
@@ -2,6 +2,23 @@ const Create = () => {
     return (
         <div className="create">
             <h2>Add a new blog</h2>
+            <form>
+                <label>Blog title</label>
+                <input
+                    type="text"
+                    required
+                />
+                <lable>Blog body:</lable>
+                <textarea
+                    required>
+                </textarea>
+                <label>Blog author:</label>
+                <select>
+                    <option value="DeeKay">DeeKay</option>
+                    <option value="TeeJay">TeeJay</option>
+                </select>
+                <button>Add blog</button>
+            </form>
         </div>
     );
 }
