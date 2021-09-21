47b71bd424a83f2a5675a7ef371faaed7c68c77d
Devin Godage <devinkanilka@gmail.com>
2021-09-22 01:55:37 +0530

    create dynamically value populate funcs added

--- src/create.js
+++ src/create.js
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('DeeKay');
+
     return (
         <div className="create">
             <h2>Add a new blog</h2>
@@ -7,13 +13,21 @@ const Create = () => {
                 <input
                     type="text"
                     required
+                    value={title}
+                    onChange={(e) => { setTitle(e.target.value); }}
                 />
                 <lable>Blog body:</lable>
                 <textarea
-                    required>
+                    required
+                    value={body}
+                    onChange={(e) => { setBody(e.target.value); }}
+                >
                 </textarea>
                 <label>Blog author:</label>
-                <select>
+                <select
+                    value={author}
+                    onChange={(e) => { setAuthor(e.target.value); }}
+                >
                     <option value="DeeKay">DeeKay</option>
                     <option value="TeeJay">TeeJay</option>
                 </select>
