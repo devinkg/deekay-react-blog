c56bc0d1321beb663cc1807e0c75e37916020522
Devin Godage <devinkanilka@gmail.com>
2021-09-23 02:20:32 +0530

    POST method added

--- src/create.js
+++ src/create.js
@@ -4,11 +4,22 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('DeeKay');
+    const [isPending, setIsPending] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const blog = { title, body, author };
-        console.log(blog);
+
+        setIsPending(true);
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('new blog added');
+            setIsPending(false);
+        })
     }
 
     return (
@@ -37,7 +48,8 @@ const Create = () => {
                     <option value="DeeKay">DeeKay</option>
                     <option value="TeeJay">TeeJay</option>
                 </select>
-                <button>Add blog</button>
+                {!isPending && <button>Add blog</button>}
+                {isPending && <button disabled>Adding blog...</button>}
             </form>
         </div>
     );
